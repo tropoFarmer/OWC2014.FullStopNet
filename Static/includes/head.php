9e368b7a675d03938747041ec731d947be8ef123
Matt Aho <maho@nerdery.com>
2014-04-12 11:08:41 -0500

    Added basic includes

---
+++ Static/includes/head.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en" class="no-js">
+<head>
+    <!-- META DATA -->
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <!--[if IE]>
+    <meta name="MSSmartTagsPreventParsing" content="true" />
+    <meta http-equiv="imagetoolbar" content="no" />
+    <meta http-equiv="cleartype" content="on" />
+    <![endif]-->
+
+    <!-- SEO -->
+    <title>Metro Blooms</title>
+
+    <!-- ICONS -->
+    <link rel="shortcut icon" type="image/ico" href="assets/images/favicon.ico" />
+
+    <!-- STYLESHEETS -->
+    <link rel="stylesheet" media="screen, projection" href="assets/styles/screen.css" />
+    <link rel="stylesheet" media="all and (min-width: 640px)" href="assets/styles/screen-medium.css" />
+    <link rel="stylesheet" media="all and (min-width: 1024px)" href="assets/styles/screen-large.css" />
+
+    <!-- JQUERY -->
+    <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+    <script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="assets/scripts/jquery.min.js"><\/script>')</script>
+
+    <!-- JAVASCRIPT -->
+    <script type="text/javascript" src="assets/scripts/global.js"></script>
+</head>
--- Static/index.php
+++ Static/index.php
@@ -1,34 +1,6 @@
-<!DOCTYPE html>
-<html lang="en" class="no-js">
-    <head>
-        <!-- META DATA -->
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <!--[if IE]>
-        <meta name="MSSmartTagsPreventParsing" content="true" />
-        <meta http-equiv="imagetoolbar" content="no" />
-        <meta http-equiv="cleartype" content="on" />
-        <![endif]-->
+<?php include 'includes/head.php'; ?>
+<?php include 'includes/header.php'; ?>
 
-        <!-- SEO -->
-        <title>Metro Blooms</title>
+Index
 
-        <!-- ICONS -->
-        <link rel="shortcut icon" type="image/ico" href="assets/images/favicon.ico" />
-
-        <!-- STYLESHEETS -->
-        <link rel="stylesheet" media="screen, projection" href="assets/styles/screen.css" />
-        <link rel="stylesheet" media="all and (min-width: 640px)" href="assets/styles/screen-medium.css" />
-        <link rel="stylesheet" media="all and (min-width: 1024px)" href="assets/styles/screen-large.css" />
-
-        <!-- JQUERY -->
-        <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
-        <script type="text/javascript">window.jQuery || document.write('<script type="text/javascript" src="assets/scripts/jquery.min.js"><\/script>')</script>
-
-        <!-- JAVASCRIPT -->
-        <script type="text/javascript" src="assets/scripts/global.js"></script>
-    </head>
-<body>
-
-</body>
-</html>
+<?php include 'includes/footer.php'; ?>
