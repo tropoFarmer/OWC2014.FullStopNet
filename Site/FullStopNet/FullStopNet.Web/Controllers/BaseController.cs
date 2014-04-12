4a4b662b078d4a8e0192738d44506becd73db280
Wade Kallhoff <wkallhof@nerdery.com>
2014-04-12 13:46:53 -0500

    Updated with functional controllers and views

---
+++ Site/FullStopNet/FullStopNet.Web/Controllers/BaseController.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+
+namespace FullStopNet.Web.Controllers
+{
+    public class BaseController : RenderMvcController
+    {
+    }
+}
