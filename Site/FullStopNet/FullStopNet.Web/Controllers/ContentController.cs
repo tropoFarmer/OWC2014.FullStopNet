ef127b2ae8c61678cbb6cfd8d89300e9a6943a91
Wade Kallhoff <wkallhof@nerdery.com>
2014-04-12 14:27:34 -0500

    Adding SEO

---
+++ Site/FullStopNet/FullStopNet.Web/Controllers/ContentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using umbraco;
+using FullStopNet.Web.ViewModels;
+
+namespace FullStopNet.Web.Controllers
+{
+    public class ContentController : SurfaceController
+    {
+        public ActionResult Head()
+        {
+            var currentNode = uQuery.GetCurrentNode();
+            return PartialView(new SEO(currentNode));
+        }
+    }
+}
