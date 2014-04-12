24774885b39c5f15b6fcd9d1bd16245c113b29c2
David Ly <davy803@gmail.com>
2014-04-12 17:33:45 -0500

    Added initial API integration

---
+++ Site/MetroBlooms/MetroBlooms.Api/JsonConverters/BoolConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+using Newtonsoft.Json;
+
+namespace MetroBlooms.Api.JsonConverters
+{
+    public class BoolConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            writer.WriteValue(((bool)value) ? 1 : 0);
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            if (objectType == typeof(bool))
+            {
+                return reader.Value != null && reader.Value.ToString() == "1";
+            }
+            if (objectType == typeof(bool?))
+            {
+                return reader.Value == null ? (bool?)null : reader.Value.ToString() == "1";
+            }
+            throw new ArgumentException("Invalid type for converter", "objectType");
+        }
+
+        public override bool CanConvert(Type objectType)
+        {
+            return objectType == typeof(bool) || objectType == typeof(bool?);
+        }
+    }
+}
---
+++ Site/MetroBlooms/MetroBlooms.Api/RemoteApi/ApiBase.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+using MetroBlooms.Api.ViewModels;
+
+using Newtonsoft.Json;
+
+namespace MetroBlooms.Api.RemoteApi
+{
+    public abstract class ApiBase
+    {
+        protected ApiBase(string userName, string password)
+        {
+            UserName = userName;
+            Password = password;
+        }
+
+        protected const string RootUrl = "http://www.metroblooms.org/api/";
+        protected string SessionToken { get; set; }
+
+        protected List<T> GetList<T>(string relativeUrl)
+        {
+            Authenticate();
+            using (var webClient = new WebClient())
+            {
+                var stringResult = webClient.DownloadString(string.Format("{0}{1}?token={2}", RootUrl, relativeUrl, SessionToken));
+                var result = JsonConvert.DeserializeObject<List<T>>(stringResult);
+                return result;
+            }
+        }
+
+        protected T GetSingleFromList<T>(string relativeUrl)
+        {
+            return GetList<T>(relativeUrl).FirstOrDefault();
+        }
+
+        protected void Authenticate()
+        {
+            if (SessionToken != null)
+            {
+                return;
+            }
+
+            using (var wc = new WebClient())
+            {
+                wc.Headers[HttpRequestHeader.ContentType] = "application/x-www-form-urlencoded";
+
+                var htmlResult = wc.UploadString(RootUrl + "auth/login", string.Format("user={0}&pwd={1}", UserName, Password));
+                var result = JsonConvert.DeserializeObject<LoginResult>(htmlResult);
+                SessionToken = result.SessionToken;
+            }
+        }
+
+        private string UserName { get; set; }
+        private string Password { get; set; }
+    }
+}
