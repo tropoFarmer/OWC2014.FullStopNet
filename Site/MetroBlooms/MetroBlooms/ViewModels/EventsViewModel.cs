7f192681bea7547b63dae1880c206fdf46c44b98
David Ly <davy803@gmail.com>
2014-04-13 07:41:56 -0500

    Events page

--- Site/MetroBlooms/MetroBlooms/ViewModels/EventsViewModel.cs
+++ Site/MetroBlooms/MetroBlooms/ViewModels/EventsViewModel.cs
@@ -3,11 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 
 using MetroBlooms.Api.ViewModels;
+using MetroBlooms.ViewModels.Generic;
+
+using umbraco.NodeFactory;
 
 namespace MetroBlooms.ViewModels
 {
-    public class EventsViewModel : UmbracoView
+    public class EventsViewModel : GenericViewModel
     {
+        public EventsViewModel(Node contextNode) : base(contextNode)
+        {
+        }
+
         public List<Event> Events { get; set; }
     }
 }
