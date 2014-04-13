7ea5623bf097a6144e9cf51d03e79c8e59fe7206
Wade Kallhoff <wkallhof@nerdery.com>
2014-04-12 22:12:36 -0500

    Updating with sections

---
+++ Site/MetroBlooms/MetroBlooms/ViewModels/Sections/BaseSection.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.NodeFactory;
+
+namespace MetroBlooms.ViewModels.Sections
+{
+    /// <summary>
+    /// Base Section used to set the Node context for
+    /// pulling section properties from
+    /// </summary>
+    public class BaseSection
+    {
+        public Node NodeContext { get; set; }
+        public BaseSection(Node node)
+        {
+            this.NodeContext = node;
+        }
+    }
+}
---
+++ Site/MetroBlooms/MetroBlooms/ViewModels/Sections/ContentWithImageSection.cs
@@ -0,0 +1,30 @@
+﻿using MetroBlooms.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.NodeFactory;
+using umbraco;
+using MetroBlooms.Extensions;
+
+
+namespace MetroBlooms.ViewModels.Sections
+{
+    public class ContentWithImageSection : BaseSection
+    {
+        public string Text { get; set; }
+        public UmbracoImage Image { get; set; }
+        public string ImageLocation { get; set; }
+        public string ImageSize { get; set; }
+
+        public ContentWithImageSection(Node node) : base(node)
+        {
+            if (node == null) return;
+
+            this.Text = node.GetProperty<string>("text");
+            this.Image = node.GetImage("image");
+            this.ImageLocation = node.GetProperty<string>("imageLocation");
+            this.ImageSize = node.GetProperty<string>("imageSize");
+        }
+    }
+}
