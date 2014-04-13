662c9a873c4e63c3a6c26fb9a12f8f0bbc9404e8
David Ly <davy803@gmail.com>
2014-04-13 06:48:41 -0500

    Start of event API integration

---
+++ Site/MetroBlooms/MetroBlooms.Api/ViewModels/Event.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MetroBlooms.Api.ViewModels
+{
+    public class Event
+    {
+        public string Id { get; set; }
+        public string Workshop_Desc_Id { get; set; }
+        public string Type { get; set; }
+        public string Date { get; set; }
+        public DateTime? Endtime { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public Location Location { get; set; }
+    }
+}
---
+++ Site/MetroBlooms/MetroBlooms/ViewModels/EventsViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using MetroBlooms.Api.ViewModels;
+
+namespace MetroBlooms.ViewModels
+{
+    public class EventsViewModel : UmbracoView
+    {
+        public List<Event> Events { get; set; }
+    }
+}
