40660af881d4076d8788432b9a64525423498cc0
Matt Aho <maho@nerdery.com>
2014-04-13 00:20:37 -0500

    Styled header

--- Static/includes/header.php
+++ Static/includes/header.php
@@ -5,23 +5,21 @@ if (!isset($footerType)) {
 ?>
 <div class="site">
     <div class="siteHeader" role="masthead">
-        <div class="container">
-            <div class="headerInfo">
-                <a class="signIn" href="#">Sign In</a>
-                <ul class="navSecondary">
-                    <li><a href="#">Upcoming Events</a></li>
-                    <li><a href="#">About Us</a></li>
-                </ul>
-            </div>
-            <div class="headerLower">
-                <a class="headerLogo" href="#"><img src="assets/images/logo.png" alt="Metro Blooms"/></a>
-                <ul class="nav">
-                    <li><a href="#"><span class="navTag">Learn about</span> Raingardens</a></li>
-                    <li><a href="#"><span class="navTag">Learn about</span> Get Involved</a></li>
-                    <li><a href="#"><span class="navTag">Learn about</span> Nominate a Garden</a></li>
-                    <li><a href="#"><span class="navTag">Learn about</span> Evaluate your Garden</a></li>
-                </ul>
-            </div>
-        </div> <!-- /.container -->
+        <div class="headerInfo">
+            <a class="signIn" href="#">Sign In</a>
+            <ul class="navSecondary">
+                <li><a href="#">Upcoming Events</a></li>
+                <li><a href="#">About Us</a></li>
+            </ul>
+        </div>
+        <div class="headerLower">
+            <a class="headerLogo" href="#"><img src="assets/images/logo.png" alt="Metro Blooms"/></a>
+            <ul class="nav">
+                <li><a href="#"><span class="navTag">Learn about</span> Raingardens</a></li>
+                <li><a href="#"><span class="navTag">How to Help</span> Get Involved</a></li>
+                <li><a href="#"><span class="navTag">Award Entries</span> Submit a Garden</a></li>
+                <li><a href="#"><span class="navTag">Award Judging</span> Evaluate a Garden</a></li>
+            </ul>
+        </div>
     </div> <!-- /.siteHeader -->
     <div class="siteMain<?php if ($footerType == 'home') {echo " siteMain_home";} ?>" role="main">
