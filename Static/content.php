70d219d19bdb3dc4b4d89aa06fee3408dafe431b
Matt Aho <maho@nerdery.com>
2014-04-12 21:10:42 -0500

    Static - added columns

---
+++ Static/content.php
@@ -0,0 +1,17 @@
+<?php include 'includes/head.php'; ?>
+<?php include 'includes/header.php'; ?>
+
+<div class="section">
+    <div class="container">
+        <div class="grid">
+            <div class="grid-col grid-col_secondary">
+                Left Col
+            </div>
+            <div class="grid-col grid-col_primary">
+                Right Col
+            </div>
+        </div>
+    </div> <!-- /.container -->
+</div> <!-- /.section -->
+
+<?php include 'includes/footer.php'; ?>
