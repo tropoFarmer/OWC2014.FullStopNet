7f2e5ee8afb4bfe60573ef61d742ab9a96c5170e
Zach Iniguez <ziniguez@nerdery.com>
2014-04-13 07:23:28 -0500

    Added text to splash content section

--- Site/MetroBlooms/MetroBlooms/ViewModels/Sections/SplashContentWithImageSection.cs
+++ Site/MetroBlooms/MetroBlooms/ViewModels/Sections/SplashContentWithImageSection.cs
@@ -9,6 +9,7 @@ namespace MetroBlooms.ViewModels.Sections
     public class SplashContentWithImageSection : BaseSection
     {
         public UmbracoPropertyString Title { get; set; }
+        public string Text { get; set; }
         public UmbracoImage BackgroundImage { get; set; }
         public UrlPickerState CTA1 { get; set; }
         public UrlPickerState CTA2 { get; set; }
@@ -20,6 +21,7 @@ namespace MetroBlooms.ViewModels.Sections
             if (node == null) return;
 
             Title = new UmbracoPropertyString(node, "title");
+            Text = new UmbracoPropertyString(node, "text");
             BackgroundImage = node.GetImage("backgroundImage");
             CTA1 = UrlPickerState.Deserialize(node.GetProperty<string>("cta1"));
             CTA2 = UrlPickerState.Deserialize(node.GetProperty<string>("cta2"));
