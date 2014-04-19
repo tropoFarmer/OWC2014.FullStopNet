9d9c8dd7276658a33e5156c53c90e2664460f08a
Zach Iniguez <ziniguez@nerdery.com>
2014-04-19 14:18:44 -0500

    Wired up home sections

--- Site/MetroBlooms/MetroBlooms/ViewModels/Sections/CallToActionSection.cs
+++ Site/MetroBlooms/MetroBlooms/ViewModels/Sections/CallToActionSection.cs
@@ -1,6 +1,5 @@
 ﻿using MetroBlooms.Extensions;
 using MetroBlooms.Utilities;
-using MetroBlooms.ViewModels.Home;
 using uComponents.DataTypes.UrlPicker.Dto;
 using umbraco;
 using umbraco.NodeFactory;
@@ -10,22 +9,22 @@ namespace MetroBlooms.ViewModels.Sections
     public class CallToActionSection : BaseSection
     {
         public UmbracoPropertyString Title { get; set; }
-        public UmbracoImage BackgroundImage { get; set; }
-        public string BackgroundColor { get; set; }
         public UmbracoPropertyString Text { get; set; }
         public UrlPickerState CTA { get; set; }
         public string Alignment { get; set; }
+        public UmbracoImage Image { get; set; }
+        public string ImageSize { get; set; }
 
         public CallToActionSection(Node node) : base(node)
         {
             if (node == null) return;
 
             Title = new UmbracoPropertyString(node, "title");
-            BackgroundImage = node.GetImage("backgroundImage");
-            BackgroundColor = node.GetProperty<string>("backgroundColor");
             Text = new UmbracoPropertyString(node, "text");
             CTA = UrlPickerState.Deserialize(node.GetProperty<string>("cta"));
-            Alignment = node.GetProperty<string>("Alignment");
+            Alignment = node.GetProperty<string>("alignment");
+            Image = node.GetImage("image");
+            ImageSize = node.GetProperty<string>("imageSize");
         }
     }
 }
--- Site/MetroBlooms/MetroBlooms/ViewModels/Sections/SplashContentWithImageSection.cs
+++ Site/MetroBlooms/MetroBlooms/ViewModels/Sections/SplashContentWithImageSection.cs
@@ -9,11 +9,12 @@ namespace MetroBlooms.ViewModels.Sections
     public class SplashContentWithImageSection : BaseSection
     {
         public UmbracoPropertyString Title { get; set; }
-        public string Text { get; set; }
-        public UmbracoImage BackgroundImage { get; set; }
+        public UmbracoPropertyString Text { get; set; }
         public UrlPickerState CTA { get; set; }
+        public UmbracoImage BackgroundImage { get; set; }
         public string BackgroundColor { get; set; }
-        public string Size { get; set; }
+        public string SectionSize { get; set; }
+        public string TitleSize { get; set; }
         public string Alignment { get; set; }
         public string Theme { get; set; }
         
@@ -25,9 +26,10 @@ namespace MetroBlooms.ViewModels.Sections
             BackgroundImage = node.GetImage("backgroundImage");
             BackgroundColor = node.GetProperty<string>("backgroundColor");
             CTA = UrlPickerState.Deserialize(node.GetProperty<string>("cta"));
-            Size = node.GetProperty<string>("Size");
-            Alignment = node.GetProperty<string>("Alignment");
-            Theme = node.GetProperty<string>("Theme");
+            TitleSize = node.GetProperty<string>("titleSize");
+            SectionSize = node.GetProperty<string>("sectionSize");
+            Alignment = node.GetProperty<string>("alignment");
+            Theme = node.GetProperty<string>("theme");
         }
     }
 }
